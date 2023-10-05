5e982ddcde5da778f687701fae84dff2a6c32f18
AmazingConnie <rmallu21@gmail.com>
2023-10-05 19:54:20 -0400

    added to <p></p>

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+// IMPORT
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Hello World, this is my first Next.js app!
+          Hello World, this is my first Next.js app! See you soon!
         </p>
         <div>
           <a
